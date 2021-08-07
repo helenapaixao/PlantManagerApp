003a4566301568b3369c45407d7ca15bf3e97297
Helena Paixão <hp.helenapaixao@gmail.com>
2021-08-06 23:27:15 -0300

    fix: pages navigation:ok

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,34 +1,37 @@
 import React from "react";
 import { Text, Image } from "react-native";
 import wateringImg from "../../assets/watering.png";
-import Button from '../../components/Button'
-import { Feather } from '@expo/vector-icons';
+import Button from "../../components/Button";
+import { Feather } from "@expo/vector-icons";
 import * as S from "./styles";
+import {useNavigation} from '@react-navigation/core'
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+
+  function handleStart() {
+    navigation.navigate('UserIdentification')
+  }
   return (
     <>
-    <S.Container>
-      <S.Title>
-        Gerencie {"\n"} suas plantas de {"\n"} forma fácil
-      </S.Title>
-      <Image source={wateringImg} resizeMode="contain" />
+      <S.Container>
+        <S.Title>
+          Gerencie {"\n"} suas plantas de {"\n"} forma fácil
+        </S.Title>
+        <Image source={wateringImg} resizeMode="contain" />
 
-      <S.SubTitle>
-        Não esqueça mais de regar suas {"\n"} plantas. Nós cuidamos de lembrar
-        você sempre que precisar.
-      </S.SubTitle>
-       <S.ButtonSubmit
-       activeOpacity={0.7}
-    
-      >
-          <Feather name="chevron-right"  />
-      
-      </S.ButtonSubmit>
-    </S.Container>
-    
-
-      </>
+        <S.SubTitle>
+          Não esqueça mais de regar suas {"\n"} plantas. Nós cuidamos de lembrar
+          você sempre que precisar.
+        </S.SubTitle>
+        <S.ButtonSubmit activeOpacity={0.7}
+           onPress={handleStart}
+        >
+          <Feather name="chevron-right" />
+        </S.ButtonSubmit>
+      </S.Container>
+    </>
   );
 }
 
